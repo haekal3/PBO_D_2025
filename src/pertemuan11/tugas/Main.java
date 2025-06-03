5ef0753744c2c8ee001610b6c72ede8c6919a71c
haekal3 <haekalkhadafi21@gmail.com>
2025-06-03 23:20:25 +0700

    tugas

--- src/pertemuan11/tugas/Main.java
+++ src/pertemuan11/tugas/Main.java
@@ -1,15 +1,15 @@
 package pertemuan11.tugas;
 
 public class Main {
- public static void main(String[] args) {
-     EWallet ewallet = new EWallet(50000, 100000);
-     ewallet.proses();
-     ewallet.cetakBukti();
-
-     System.out.println();
-
-     KartuKredit kartu = new KartuKredit(150000, 100000);
-     kartu.proses();
-     kartu.cetakBukti();
- }
+	 public static void main(String[] args) {
+	     EWallet ewallet = new EWallet(50000, 100000);
+	     ewallet.proses();
+	     ewallet.cetakBukti();
+	
+	     System.out.println();
+	
+	     KartuKredit kartu = new KartuKredit(150000, 100000);
+	     kartu.proses();
+	     kartu.cetakBukti();
+	 }
 }
